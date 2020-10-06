31dad1723a33cc483e532d3569d54822416bee63
Tony Yeh <yeh0935135650@gmail.com>
2020-10-06 21:31:09 +0800

    add check file

--- products.py
+++ products.py
@@ -1,12 +1,17 @@
-
-
+import os
 products = []
-with open('products.csv', 'r', encoding= 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price  = line.strip().split(',')
-		products.append([name,price])
+if os.path.isfile('products.csv'):
+	with open('products.csv', 'r', encoding= 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price  = line.strip().split(',')
+			products.append([name,price])
+else:
+	print('第一次輸入')
+
+
+
 while True:
 	name = input('請輸入商品名稱，輸入完畢請輸入q： ',)
 	if name == 'q':
